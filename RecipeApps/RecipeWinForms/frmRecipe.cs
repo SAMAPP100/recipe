de52f5d583ab9cc4d2b1850e0b977c5d3f693a27
S Breuer <support@app100.co.uk>
2023-03-21 13:19:54 +0000

    Added CRUD

--- RecipeApps/RecipeWinForms/frmRecipe.cs
+++ RecipeApps/RecipeWinForms/frmRecipe.cs
@@ -1,28 +1,88 @@
 ﻿using CPUFrameWork;
 using System.Data;
+using System.Diagnostics;
 
 namespace RecipeWinForms
 {
     public partial class frmRecipe : Form
     {
+        DataTable dtrecipes;
         public frmRecipe()
         {
             InitializeComponent();
+            btnSave.Click += BtnSave_Click;
+            btnDelete.Click += BtnDelete_Click;
         }
+
+
+
         public void ShowForm(int recipeid)
         {
-            string sql = "SELECT  r.RecipeID, r.RecipeName, c.CuisineDesc, u.UserName, r.Calories, r.DraftDate, r.PublishDate, r.ArchiveDate, r.RecipeImageName  FROM Recipe r  JOIN Cuisine c   ON r.CuisineID = c.CuisineID  JOIN Users u  ON r.UserID = u.UserID where r.RecipeID = " + recipeid.ToString();
-            DataTable dt = SQLUtility.GetDataTable(sql);
-            txtRecipeName.DataBindings.Add("Text", dt, "RecipeName");
-            lblCusine.DataBindings.Add("Text", dt, "CuisineDesc");
-            txtCalories.DataBindings.Add("Text", dt, "Calories");
-            txtRecipeImageName.DataBindings.Add("Text", dt, "RecipeImageName");
-            lblUser.DataBindings.Add("Text", dt, "UserName");
-            txtDraftDate.DataBindings.Add("Text", dt, "DraftDate");
-            txtPublishDate.DataBindings.Add("Text", dt, "PublishDate");
-            txtArchiveDate.DataBindings.Add("Text", dt, "ArchiveDate");
+            string sql = "SELECT  r.RecipeID, r.RecipeName, r.CuisineID, r.UserID, r.Calories, r.DraftDate, r.PublishDate, r.ArchiveDate, r.RecipeImageName  FROM Recipe r where r.RecipeID = " + recipeid.ToString();
+            dtrecipes = SQLUtility.GetDataTable(sql);
+            if (recipeid == 0)
+            {
+                dtrecipes.Rows.Add();
+            }
+            DataTable tdcuisine = SQLUtility.GetDataTable("SELECT  CuisineId, CuisineDesc  FROM Cuisine");
+            WindowsFormsUtility.SetListBinding(lstCuisineDesc, tdcuisine, dtrecipes, "Cuisine");
+
+            DataTable tdusers = SQLUtility.GetDataTable("SELECT UserID, UserName  FROM Users");
+            WindowsFormsUtility.SetListBinding(lstUserName, tdusers, dtrecipes, "Users", "UserID");
+
+            WindowsFormsUtility.SetControlBinding(txtRecipeName, dtrecipes);
+            WindowsFormsUtility.SetControlBinding(txtCalories, dtrecipes);
+            WindowsFormsUtility.SetControlBinding(txtRecipeImageName, dtrecipes);
+            WindowsFormsUtility.SetControlBinding(txtDraftDate, dtrecipes);
+            WindowsFormsUtility.SetControlBinding(txtPublishDate, dtrecipes);
+            WindowsFormsUtility.SetControlBinding(txtArchiveDate, dtrecipes);
+
             this.Show();
         }
 
+        private void Save()
+        {
+            DataRow r = dtrecipes.Rows[0];
+            int id = (int)r["RecipeID"];
+
+            string sql;
+            // I was instructed not to update the dates because I was having date format issues.
+            if (id > 0)
+            {
+
+                sql = string.Join(Environment.NewLine, $"update Recipe set",
+                    $" RecipeName = '{r["RecipeName"]}',",
+                    $" CuisineID = '{r["CuisineID"]}',",
+                    $" Calories = '{r["Calories"]}'",
+                    $"where RecipeID = {r["RecipeID"]} ");
+            }
+            else
+            {
+                sql = "insert Recipe(RecipeName, CuisineID, Calories, UserID) ";
+                sql += $"select '{r["RecipeName"]}', {r["CuisineID"]}, '{r["Calories"]}', '{r["UserID"]}'";
+            }
+
+
+
+            Debug.Print(sql);
+            SQLUtility.ExecuteSQL(sql);
+        }
+
+        private void Delete()
+        {
+            int id = (int)dtrecipes.Rows[0]["RecipeID"];
+            string sql = "delete Recipe where RecipeID = " + id;
+            SQLUtility.ExecuteSQL(sql);
+            this.Close();
+        }
+
+        private void BtnSave_Click(object? sender, EventArgs e)
+        {
+            Save();
+        }
+        private void BtnDelete_Click(object? sender, EventArgs e)
+        {
+            Delete();
+        }
     }
 }
